59667d62be80b8294a305684dad413df73f383e3
Aaiman Momin <63570426+AaimanMomin@users.noreply.github.com>
2025-09-22 08:24:42 +0530

    Longest SubString Length && Valid Paranthesis

--- LongestSubStringLength.java
+++ LongestSubStringLength.java
@@ -1,2 +1,25 @@
-public class longestSubStringLength {
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestSubStringLength {
+
+    public static void main(String args[]){
+        String s = "abcabcbb";
+        System.out.println(longestSubstringLen(s));
+    }
+
+    public static int longestSubstringLen(String s){
+        int maxlen = 0;
+        int left = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        for(int right = 0; right < s.length(); right++){
+            char c = s.charAt(right);
+            if(map.containsKey(c) && map.get(c) >= left){
+                left = map.get(c) + 1;
+            }
+            maxlen = Math.max(maxlen, right-left +1);
+            map.put(c, right);
+        }
+        return maxlen;
+    }
 }
